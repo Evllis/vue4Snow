2a449cb5bd9214a7d626ae9bc753614ed434037b
Evllis <7284395@qq.com>
2020-11-26 17:43:47 +0800

    🎉 tada(新项目): 基于vue4开发新项目

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+const plugins = [
+    ['import', {
+        libraryName: 'vant',
+        libraryDirectory: 'es',
+        style: true
+    }, 'vant']
+]
+
+// 生产环境移除console
+if (process.env.NODE_ENV === 'production') {
+    plugins.push("transform-remove-console")
+}
+
+module.exports = {
+    presets: [
+        '@vue/cli-plugin-babel/preset'
+    ],
+    plugins,
+}
